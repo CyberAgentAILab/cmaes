50d7caf869546b7012e7b18ca99e91d9062a3853
c-bata <contact@c-bata.link>
2020-07-23 11:11:49 +0900

    Set FutureWarning category

--- cmaes/cma.py
+++ cmaes/cma.py
@@ -5,4 +5,5 @@ from ._cma import CMA  # NOQA
 warnings.warn(
     "This module is deprecated. Please import CMA class from the "
     "package root (ex: from cmaes import CMA).",
+    FutureWarning,
 )
