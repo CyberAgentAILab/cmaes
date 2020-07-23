9c0c8f3c4637d002fb3237a9aec74867c1b4299b
c-bata <contact@c-bata.link>
2020-07-23 11:06:56 +0900

    Fix lint errors

--- cmaes/cma.py
+++ cmaes/cma.py
@@ -1,9 +1,8 @@
 import warnings
 
-from ._cma import CMA
+from ._cma import CMA  # NOQA
 
 warnings.warn(
     "This module is deprecated. Please import CMA class from the "
     "package root (ex: from cmaes import CMA).",
-    DeprecationWarning,
 )
