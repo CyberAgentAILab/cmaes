eba64233ea22c6e885a2af885ff63e9abd56ee9f
c-bata <contact@c-bata.link>
2020-07-24 20:57:02 +0900

    Refactor tests

--- tests/test_termination_criterion.py
+++ tests/test_termination_criterion.py
@@ -1,6 +1,5 @@
-import random
-
 import numpy as np
+
 from unittest import TestCase
 from cmaes import CMA
 
@@ -11,19 +10,17 @@ class TestTerminationCriterion(TestCase):
         popsize = optimizer.population_size
         rng = np.random.RandomState(seed=1)
 
-        for i in range(optimizer._funhist_term):
-            optimizer.tell([(rng.randn(2), 0.01) for _ in range(popsize)])
+        for i in range(optimizer._funhist_term + 1):
             self.assertFalse(optimizer.should_stop())
+            optimizer.tell([(rng.randn(2), 0.01) for _ in range(popsize)])
 
-        optimizer.tell([(rng.randn(2), 0.01) for _ in range(popsize)])
         self.assertTrue(optimizer.should_stop())
 
     def test_stop_if_detect_divergent_behavior(self):
-        optimizer = CMA(mean=np.zeros(2), sigma=1e-8)
+        optimizer = CMA(mean=np.zeros(2), sigma=1e-4)
         popsize = optimizer.population_size
-        rng = random.Random(1)
         nd_rng = np.random.RandomState(1)
 
-        solutions = [(nd_rng.randn(2), 1000 * rng.random()) for _ in range(popsize)]
+        solutions = [(100 * nd_rng.randn(2), 0.01) for _ in range(popsize)]
         optimizer.tell(solutions)
         self.assertTrue(optimizer.should_stop())
