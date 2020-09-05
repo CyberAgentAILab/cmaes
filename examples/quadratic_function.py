982af28bb17d3ed3929e75306754f3dac508b868
c-bata <contact@c-bata.link>
2020-09-05 21:28:09 +0900

    Revert "Merge pull request #75 from CyberAgent/drop-python35"
    This reverts commit 438c469336503cd616a7228be02d0d4f2f5c9576, reversing
    changes made to 73ea8a74e9692a3b8abc05bf33cd7f4a7ab26da6.

--- examples/quadratic_function.py
+++ examples/quadratic_function.py
@@ -17,10 +17,11 @@ def main():
             x = optimizer.ask()
             value = quadratic(x[0], x[1])
             solutions.append((x, value))
-            print(
-                f"{optimizer.generation:3d}  {value:10.5f}"
-                f"  {x[0]:6.2f}  {x[1]:6.2f}"
+
+            msg = "{g:3d}  {value:10.5f}  {x1:6.2f}  {x2:6.2f}".format(
+                g=optimizer.generation, value=value, x1=x[0], x2=x[1],
             )
+            print(msg)
         optimizer.tell(solutions)
 
         if optimizer.should_stop():
