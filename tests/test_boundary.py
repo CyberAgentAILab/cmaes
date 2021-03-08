f5626eab4195844484d44377e059eb4ad4d0b6d3
c-bata <contact@c-bata.link>
2021-03-08 11:30:31 +0900

    Validate bounds domain contains mean

--- tests/test_boundary.py
+++ tests/test_boundary.py
@@ -11,6 +11,13 @@ class TestCMABoundary(TestCase):
         with self.assertRaises(AssertionError):
             CMA(mean=np.zeros(2), sigma=1.3, bounds=np.array([-10, 10]))
 
+    def test_mean_located_out_of_bounds(self):
+        mean = np.zeros(5)
+        bounds = np.empty(shape=(5, 2))
+        bounds[:, 0], bounds[:, 1] = 1.0, 5.0
+        with self.assertRaises(AssertionError):
+            CMA(mean=mean, sigma=1.3, bounds=bounds)
+
     def test_set_valid_bounds(self):
         optimizer = CMA(mean=np.zeros(2), sigma=1.3)
         optimizer.set_bounds(bounds=np.array([[-10, 10], [-10, 10]]))
@@ -19,3 +26,10 @@ class TestCMABoundary(TestCase):
         optimizer = CMA(mean=np.zeros(2), sigma=1.3)
         with self.assertRaises(AssertionError):
             optimizer.set_bounds(bounds=np.array([-10, 10]))
+
+    def test_set_bounds_which_does_not_contains_mean(self):
+        optimizer = CMA(mean=np.zeros(2), sigma=1.3)
+        bounds = np.empty(shape=(5, 2))
+        bounds[:, 0], bounds[:, 1] = 1.0, 5.0
+        with self.assertRaises(AssertionError):
+            optimizer.set_bounds(bounds)
