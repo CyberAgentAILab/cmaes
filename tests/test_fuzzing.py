b112c0c90720f018ec786d7a70af41990be79a57
c-bata <contact@c-bata.link>
2021-03-09 10:44:03 +0900

    Add hypothesis testing

---
+++ tests/test_fuzzing.py
@@ -0,0 +1,26 @@
+import hypothesis.extra.numpy as npst
+import unittest
+from hypothesis import given, strategies as st
+
+from cmaes import CMA
+
+
+class TestFuzzing(unittest.TestCase):
+    @given(
+        data=st.data(),
+    )
+    def test_cma_tell(self, data):
+        dim = data.draw(st.integers(min_value=2, max_value=100))
+        mean = data.draw(npst.arrays(dtype=float, shape=dim))
+        sigma = data.draw(st.floats(min_value=1e-16))
+        n_iterations = data.draw(st.integers(min_value=1))
+        optimizer = CMA(mean, sigma)
+        popsize = optimizer.population_size
+        for _ in range(n_iterations):
+            tell_solutions = data.draw(
+                st.lists(st.tuples(npst.arrays(dtype=float, shape=dim), st.floats()),
+                         min_size=popsize, max_size=popsize)
+            )
+            optimizer.ask()
+            optimizer.tell(tell_solutions)
+            optimizer.ask()
