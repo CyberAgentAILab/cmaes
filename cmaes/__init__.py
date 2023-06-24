fe30268488a9177e06529c30cb009455baa66a27
Masahiro Nomura <masahironomura5325@gmail.com>
2023-06-24 14:07:59 +0900

    add DX-NES-IC to init file

--- cmaes/__init__.py
+++ cmaes/__init__.py
@@ -2,5 +2,6 @@ from ._cma import CMA  # NOQA
 from ._sepcma import SepCMA  # NOQA
 from ._warm_start import get_warm_start_mgd  # NOQA
 from ._cmawm import CMAwM  # NOQA
+from ._dxnesic import DXNESIC  # NOQA
 
 __version__ = "0.9.1"
